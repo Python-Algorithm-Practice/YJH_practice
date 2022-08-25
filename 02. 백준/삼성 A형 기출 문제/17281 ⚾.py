3651269c3744bf3136240d473bf64d4fa0f85d85
HwiHwi <yjh200523@naver.com>
2022-08-26 02:42:51 +0900

    fix: BOJ 삼성 A형 기출 문제 (야구) 수정

--- 02. 백준/삼성 A형 기출 문제/17281 ⚾.py
+++ 02. 백준/삼성 A형 기출 문제/17281 ⚾.py
@@ -26,20 +26,6 @@ def next_permutation(arr):
     return True
 
 
-def go_next_base(base_state, count):
-    score = 0
-    for i in range(3, -1, -1):
-        if base_state[i]:
-            base_state[i] = False
-            if i + count > 3:  # 홈으로 갔을 경우
-                score += 1
-                continue
-            # 진루 처리
-            base_state[i + count] = True
-
-    return score
-
-
 def simulation(perm, innings):
     score = 0  # 총점
     order = 0  # 1번 타자부터 시작
@@ -49,25 +35,26 @@ def simulation(perm, innings):
         out_count = 0
         score_stk = []
         while out_count < 3:
-            base_state[0] = True
+            base_state[0] = True  # 타석에 들어섬
 
-            if order < 3:
-                cur_order = perm[order]
-            elif order == 3:
-                cur_order = 0
-            else:
-                cur_order = perm[order - 1]
-
-            if inning[cur_order] == 0:
+            if inning[perm[order]] == 0:  # 아웃 -> 아웃 카운트 증가
                 out_count += 1
-            else:
-                score_stk.append(inning[cur_order])
-            order = (order + 1) % 9
+            else:  # 1루타 이상 -> 안타 스택에 삽입
+                score_stk.append(inning[perm[order]])
+            order = (order + 1) % 9  # 다음 순서로 넘어감
 
-        while score_stk:
-            if score_stk[-1] > 3:
+        # 이닝이 종료되면 stack[top - 1] += stack[top] 해주며 점수 계산
+        # 만약 stack의 마지막 값이 3을 넘긴다면,
+        # 해당 주자를 포함하여 그 이전의 주자들 모두 홈에 들어왔다는 것이므로 스택의 길이만큼 점수 증가
+        # Ex) [0:top] [1, 2, 1, 2, 1] -> 이 경우 마지막 두 주자는 이닝이 끝났을 때 3, 1루에 위치해 있음
+        # 1: [1, 2, 1, 3] (+1)
+        # 2: [1, 2, 4] (+3) -> 스택의 top이 3을 넘겼으므로 스택의 모든 주자는 1점씩 득점한 것
+        # 왜? 내 뒷 주자보다 늦게 홈에 들어갈 수 없음. 다시 말해 내가 홈에 들어간다면 내 앞의 주자 모두 이미 홈에 들어갔다는 뜻
+        while score_stk:  # 이닝이 종료됐을 때 아직 들어가지 못 한 주자들 pop()
+            if score_stk[-1] > 3:  # 홈에 들어간 주자를 발견하면 그 이전 주자 모두 홈에 들어간 것이므로 점수에 스택 길이를 더함
                 score += len(score_stk)
                 break
+            # 진루 계산
             top = score_stk.pop()
             if score_stk:
                 score_stk[-1] += top
@@ -81,11 +68,12 @@ def solution():
     n = int(sys_input())
     innings = [list(map(int, sys_input().split())) for _ in range(n)]
 
-    arr = [1, 2, 3, 4, 5, 6, 7, 8]
+    arr = [1, 2, 3, 0, 4, 5, 6, 7, 8]  # 가능한 첫 순서
 
-    max_score = 0
+    max_score = simulation(arr, innings)
     while next_permutation(arr):
-        max_score = max(max_score, simulation(arr, innings))
+        if arr[3] == 0:  # 1번 선수가 4번 타자인 경우만 시뮬레이션
+            max_score = max(max_score, simulation(arr, innings))
     print(max_score)
 
 
