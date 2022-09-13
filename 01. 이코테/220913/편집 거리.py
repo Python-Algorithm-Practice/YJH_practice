52926ed92fc4afd5000551683ae99e8f7a26ae18
HwiHwi <yjh200523@naver.com>
2022-09-13 18:38:27 +0900

    fix: 22.09.13 스터디 과제 (이코테 편집거리) 수정

--- 01. 이코테/220913/편집 거리.py
+++ 01. 이코테/220913/편집 거리.py
@@ -4,34 +4,43 @@ BOJ 15483 https://www.acmicpc.net/problem/15483
 import sys
 
 
+def counting(a, a_idx, b, b_idx, memo):
+    if a_idx == len(a):
+        return len(b) - b_idx
+    elif b_idx == len(b):
+        return len(a) - a_idx
+
+    # a_idx, b_idx일 때 구했던 결과가 있다면 해당 결과 반환
+    if memo[a_idx][b_idx]:
+        return memo[a_idx][b_idx]
+
+    # 현재 각 문자가 같으면 삽입, 삭제, 교체를 할 필요없이 바로 다음 문자로 판별
+    if a[a_idx] == b[b_idx]:
+        memo[a_idx][b_idx] = counting(a, a_idx + 1, b, b_idx + 1, memo)
+        return memo[a_idx][b_idx]
+
+    # 삽입, 문자열 A의 a_idx에 b[b_idx]를 삽입했다고 가정하고 다음 함수 호출
+    min_count = counting(a, a_idx, b, b_idx + 1, memo)
+
+    # 삭제, A[a_idx]를 지웠다고 가정하고 다음 함수 호출
+    min_count = min(min_count, counting(a, a_idx + 1, b, b_idx, memo))
+
+    # 교체, A[a_idx]를 B[b_idx]로 교체했다고 가정하고 다음 함수 호출
+    min_count = min(min_count, counting(a, a_idx + 1, b, b_idx + 1, memo))
+
+    # a_idx, b_idx일 때 결과가 또 필요할 수 있으므로 따로 저장
+    memo[a_idx][b_idx] = min_count + 1
+    return memo[a_idx][b_idx]
+
+
 def solution():
     sys_input = sys.stdin.readline
 
-    a = list(sys_input().rstrip())
-    b = list(sys_input().rstrip())
+    a = sys_input().rstrip()
+    b = sys_input().rstrip()
 
-    memo = [[0] * (len(b) + 1) for _ in range(len(a) + 1)]
-
-    for a_idx in range(len(a) + 1):
-        memo[a_idx][0] = a_idx
-    for b_idx in range(len(b) + 1):
-        memo[0][b_idx] = b_idx
-
-    for a_idx in range(1, len(a) + 1):
-        for b_idx in range(1, len(b) + 1):
-            memo[a_idx][b_idx] = memo[a_idx - 1][b_idx - 1]
-            if a[a_idx - 1] != b[b_idx - 1]:
-                memo[a_idx][b_idx] = min(
-                    memo[a_idx - 1][b_idx - 1],
-                    memo[a_idx - 1][b_idx],
-                    memo[a_idx][b_idx - 1]) + 1
-
-                if a_idx > 1 and b_idx > 1 and a[a_idx - 2] == b[b_idx - 1] and a[a_idx - 1] == b[b_idx - 2]:
-                    memo[a_idx][b_idx] = min(memo[a_idx][b_idx], memo[a_idx - 2][b_idx - 2] + 1)
-                if a_idx > 1 and b_idx > 2 and a[a_idx - 2] == b[b_idx - 1] and a[a_idx - 1] == b[b_idx - 3]:
-                    memo[a_idx][b_idx] = min(memo[a_idx][b_idx], memo[a_idx - 2][b_idx - 3] + 2)
-
-    print(memo[len(a)][len(b)])
+    print(counting(a, 0, b, 0, [[0] * (len(b) + 1) for _ in range(len(a) + 1)]))
 
 
+sys.setrecursionlimit(1000000)  # 1000 * 1000이므로 최대 100만?
 solution()
